9f438d81e23fbfb088345903b08240fc7482e403
Smillerrrr <8806@holbertonstudents.com>
2024-02-20 20:45:10 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -8,7 +8,7 @@
  **/
 int main(void)
 {
-	char strg[] = "with proper grammar, but the outcome is a piece of art,";
+	char strg[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", strg);
 

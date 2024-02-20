ddcd840d5d95cec9b47fca2bf5cf49b9d6146e6a
Smillerrrr <8806@holbertonstudents.com>
2024-02-20 20:40:11 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print a string
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	char strg[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", strg);
+
+	return (0);
+}

934a1902b6d256989e2fe5da957c3fb72c40c1e8
Smillerrrr <8806@holbertonstudents.com>
2024-02-20 17:39:43 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Auth: Smillerrrr <8806@holbertonstudent.com>
+ */
+
 #include <stdio.h>
+
+/*
+ * main - prints ""Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {

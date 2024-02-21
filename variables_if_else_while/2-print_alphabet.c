d007bb1dbccf6d451d36306f329a0d4609a4bf7b
Smillerrrr <8806@holbertonstudent.com>
2024-02-21 15:11:05 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Description: for = loop
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}

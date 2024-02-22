275de5d2beb0a74c1c151318068aed41fdb0dbdb
Smillerrrr <8806@holbertonstudents.com>
2024-02-22 13:08:38 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Description: List nb
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int nb;
+
+	for (nb = 48 ; nb < 58 ; nb++)
+	{
+		putchar(nb);
+	}
+
+	putchar(10);
+
+	return (0);
+}

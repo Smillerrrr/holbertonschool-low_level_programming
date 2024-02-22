3b91047fa1fc71fc0d408b28c52ef7cb59fca95b
Smillerrrr <8806@holbertonstudents.com>
2024-02-22 22:35:07 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - print _putchar
  *
  * Description: décompose le _putchar pour l'afficher
- *Return: 0
+ * Return: 0
  **/
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	putchar('\n');
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,5 @@
-Not empty
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+#endif

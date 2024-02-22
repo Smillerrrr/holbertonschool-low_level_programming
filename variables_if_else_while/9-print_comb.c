44865668d427853043f529c34d60524acdaf550e
Smillerrrr <8806@holbertonstudents.com>
2024-02-22 16:31:23 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print number
+ *
+ * Description: Nested loop
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+
+		if (digit < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

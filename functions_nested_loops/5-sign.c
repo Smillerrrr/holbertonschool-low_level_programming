6ac3710066857acbc543422cc153fb182c5d8dd5
Smillerrrr <8806@holbertonstudent.com>
2024-02-25 00:06:47 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_sign - Affiche le signe
+ *
+ * @n: Print the sign of a number
+ * Return: 0
+ **/
+
+int print_sign(int n)
+{
+	if (n >= 0 && n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n >= 0 && n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}

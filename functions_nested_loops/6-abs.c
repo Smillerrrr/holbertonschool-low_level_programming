50a5058dcf64c4669d41f86a1643a810901ef840
Smillerrrr <8806@holbertonstudent.com>
2024-02-25 00:35:50 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - calcul
+ *
+ *@a: Calcul la valeur absolue d'un nombre
+ * Return: 0
+ **/
+
+int _abs(int a)
+
+{
+	if (a > 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (a * -1);
+	}
+}

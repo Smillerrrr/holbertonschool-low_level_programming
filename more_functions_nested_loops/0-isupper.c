d234a162a6cf2ba436259157a52066d936d52037
Smillerrrr <8806@holbertonstudents.com>
2024-02-26 01:27:48 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Write a function that checks for uppercase character
+ *
+ * @c: Maj and min
+ * Return: 1
+ **/
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+
+		return (1);
+
+	else
+
+		return (0);
+}

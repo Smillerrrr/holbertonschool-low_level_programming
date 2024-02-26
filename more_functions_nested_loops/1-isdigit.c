43bdca1413962ce65f26784b99c3782ae9ea2a6c
Smillerrrr <8806@holbertonstudents.com>
2024-02-26 01:44:33 +0000

    1-isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Write a function that checks for a digit
+ *
+ * @c: print a number
+ * Return: 0 or 1
+ **/
+
+int _isdigit(int c)
+
+{
+	if (c <= 9)
+
+		return (1);
+
+		else
+
+		return (0);
+}

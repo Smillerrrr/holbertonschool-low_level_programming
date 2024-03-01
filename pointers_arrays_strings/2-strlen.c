5c9836eeed53e7494e99064d8412a644dad406e1
Smillerrrr <8806@holbertonstudents.com>
2024-03-01 09:45:21 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Function
+ *
+ * @s: Write a function that returns the length of a string.
+ * Return: i
+ **/
+
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; i <= *s; i++)
+	{
+		;
+	}
+		return (i);
+}

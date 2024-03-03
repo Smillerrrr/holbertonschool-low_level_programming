9979f9bec7201bd5e339eda4feed1a3acbd40df8
Smillerrrr <8806@holbertonstudents.com>
2024-03-03 19:26:51 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - Write a function that prints a string, in reverse
+ *
+ * @s: Pointer
+ *
+ **/
+
+void print_rev(char *s)
+
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	{
+
+		;
+
+	}
+
+	k = i;
+
+	for (j = (k - 1); j >=  0; j--)
+
+	{
+
+		_putchar(s[j]);
+
+	}
+
+	_putchar('\n');
+
+}
+
